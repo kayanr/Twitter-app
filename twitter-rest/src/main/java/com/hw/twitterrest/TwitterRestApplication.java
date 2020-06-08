cdbbbaa0d9735e4678b37fe140291250a1e4cec2
Kayanr <kayan.reid@gmail.com>
2020-06-08 10:58:04 -0400

    Created and copied the twitter-rest folder and files using the Spring Boot initiator website.
    Created and copied the twitter-rest folder and files using the Spring Boot initiator website.

---
+++ twitter-rest/src/main/java/com/hw/twitterrest/TwitterRestApplication.java
@@ -0,0 +1,13 @@
+package com.hw.twitterrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterRestApplication.class, args);
+	}
+
+}
---
+++ twitter-rest/src/test/java/com/hw/twitterrest/TwitterRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.twitterrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
