eedd7a30321c3d7e418d47cb95c800a0843e53c8
Kayanr <kayan.reid@gmail.com>
2020-06-08 12:07:06 -0400

    Updated the TwitterRestApplication class.
    Updated the TwitterRestApplication class with the @RestController annotation.

--- twitter-rest/src/main/java/com/hw/twitterrest/TwitterRestApplication.java
+++ twitter-rest/src/main/java/com/hw/twitterrest/TwitterRestApplication.java
@@ -2,8 +2,10 @@ package com.hw.twitterrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TwitterRestApplication {
 
 	public static void main(String[] args) {
