dc4b67bfa8f163e92a10af9564e6d2ac0fc6c7b3
Kayanr <kayan.reid@gmail.com>
2020-06-08 11:36:55 -0400

    Added Contructors, getters and setters to the Tweet class.
    Added Contructors, getters and setters to the Tweet class.

--- twitter-rest/src/main/java/com/hw/twitterrest/model/Tweet.java
+++ twitter-rest/src/main/java/com/hw/twitterrest/model/Tweet.java
@@ -22,4 +22,52 @@ public class Tweet {
    private String message;
 
 
+   public Tweet(){}
+
+   public Tweet(String userName, String userHandle, String userImageUrl, String message) {
+      this.userName = userName;
+      this.userHandle = userHandle;
+      this.userImageUrl = userImageUrl;
+      this.message = message;
+   }
+
+   public long getId() {
+      return id;
+   }
+
+   public String getUserName() {
+      return userName;
+   }
+
+   public String getUserHandle() {
+      return userHandle;
+   }
+
+   public String getUserImageUrl() {
+      return userImageUrl;
+   }
+
+   public String getMessage() {
+      return message;
+   }
+
+   public void setId(long id) {
+      this.id = id;
+   }
+
+   public void setUserName(String userName) {
+      this.userName = userName;
+   }
+
+   public void setUserHandle(String userHandle) {
+      this.userHandle = userHandle;
+   }
+
+   public void setUserImageUrl(String userImageUrl) {
+      this.userImageUrl = userImageUrl;
+   }
+
+   public void setMessage(String message) {
+      this.message = message;
+   }
 }
